80019e7a55de18bdf9803faa64ac9eb417a9019a
Sanghyuck Han <sanghyuck.han@gmai.com>
2017-07-12 03:53:55 +0000

    successully executed Marius appel's work. for this, some source were changed.

--- debug_getHdf_modis.R
+++ debug_getHdf_modis.R
@@ -3,6 +3,6 @@ setwd("/home/scidb/work/scidbr_exam") # it must be changed with source directory
 source("mygetHdf.R")
 source("doOptions.R")
 source("minorFuns.R")
-debugonce(getStruc)
+debugonce(genString)
 
 hdf.download = mygetHdf("MOD13A3",begin="2000-01-01", end="2000-02-01",tileH = 12, tileV = 9,collection = "005")
